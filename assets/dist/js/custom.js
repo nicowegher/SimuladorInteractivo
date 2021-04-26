0496f137c5a629fe739d8b6b57bae6fa212b18e4
NicolasWegher <nicolas.wegher@gmail.com>
2021-04-26 09:16:09 -0300

    Agrego event listener, condicional y push de nuevo elemento de array.

--- assets/dist/js/custom.js
+++ assets/dist/js/custom.js
@@ -1,49 +1,74 @@
-// VALORES INICIALES
-let rooms = 10;
-let starter = 35;
-let advanced = 60;
-let pro = 100;
-document.getElementById("starter").innerHTML = starter;
-document.getElementById("advanced").innerHTML = advanced;
-document.getElementById("pro").innerHTML = pro;
+// PROMPT PARA OBTENER NOMBRE DEL USUARIO
+let usuario = prompt("Cual es tu nombre?");
+console.log(usuario)
+document.getElementById("usuario").innerHTML = usuario;
 
-// PRECIO DEL PLAN POR HABITACION
-let priceStarterXHab =3.5;
-let priceAdvancedXHab =6;
-let priceProXHab =10;
+let habitaciones = 10;
+console.log(habitaciones)
 
-// FUNCION CALCULAR PRECIO
-function calcularPrecio () {
-    let rooms = document.querySelector(`#rooms`) ? Number(document.querySelector(`#rooms`).value) : 1;
-    let starter = rooms * priceStarterXHab;
-    let advanced = rooms * priceAdvancedXHab;
-    let pro = rooms * priceProXHab;
-    console.log(starter);
-    console.log(advanced);
-    console.log(pro);
-    document.getElementById("starter").innerHTML = starter;
-    document.getElementById("advanced").innerHTML = advanced;
-    document.getElementById("pro").innerHTML = pro;
-}
-
-// PLANES (OBJETOS CON CLASES Y ARRAYS)
+// CLASE PARA CREAR OBJETOS (PLANES) 
 class Plan {
-    constructor (nombre, features ,callToAction){
+    constructor (nombre, features, callToAction, precioPorHab, precioPlan) {
         this.nombre = nombre;
         this.features = features;
         this.callToAction = callToAction;
+        this.precioPorHab = precioPorHab;
+        this.precioPlan = precioPlan;
+    }
+// METODO CUSTOM DENTRO DE LA CLASE PARA CALCULAR EL PRECIO DEL PLAN EN FUNCION DE LA HABITACIONES
+    calcularPrecioPlan () {
+        this.precioPlan = this.precioPorHab * habitaciones;
+        console.log(this.precioPlan);
+        switch (this.nombre) {
+            case "Starter":
+                document.getElementById("starter").innerHTML = starterPlan.precioPlan;
+            case "Advance":
+                document.getElementById("advance").innerHTML = advancePlan.precioPlan;
+            case "Pro":
+                document.getElementById("pro").innerHTML = proPlan.precioPlan;
+            }
     }
 }
 
-const starterPlan = new Plan("Starter", ["10 usuarios incluidos","2GB de espacio","Soporte por email","Acceso al centro de ayuda"],"Empieza Gratis");
-const advancedPlan = new Plan("Advanced", ["Feature 1","Feature 2","Feature 3","Feature 4"], "Empieza Ahora");
-const proPlan = new Plan("Pro", ["Feature 1","Feature 2","Feature 3","Feature 4"], "Habla con Ventas");
+// CREACION DE PLANES MEDIANTE CLASE
+const starterPlan = new Plan ("Starter", ["10 users included", "2 GB of storage", "Email support", "Help center access"], "Empieza Gratis", 3, 30);
+const advancePlan = new Plan ("Advance", ["Feature 1", "Feature 2", "Feature 3"], "Agenda una demo", 6, 60);
+const proPlan = new Plan ("Pro", ["Feature 1", "Feature 2", "Feature 3"], "Habla con ventas", 10, 100);
 
-// USO DE VARIABLES EN EL HTML
+// USO DE VARIABLES DE LOS OBJETOS EN EL HTML
 document.getElementById("starter.nombre").innerHTML = starterPlan.nombre;
-document.getElementById("advanced.nombre").innerHTML = advancedPlan.nombre;
+document.getElementById("advance.nombre").innerHTML = advancePlan.nombre;
 document.getElementById("pro.nombre").innerHTML = proPlan.nombre;
 
+document.getElementById("starter").innerHTML = starterPlan.precioPlan;
+document.getElementById("advance").innerHTML = advancePlan.precioPlan;
+document.getElementById("pro").innerHTML = proPlan.precioPlan;
+
 document.getElementById("starter.CTA").innerHTML = starterPlan.callToAction;
-document.getElementById("advanced.CTA").innerHTML = advancedPlan.callToAction;
+document.getElementById("advance.CTA").innerHTML = advancePlan.callToAction;
 document.getElementById("pro.CTA").innerHTML = proPlan.callToAction;
+
+starterPlan.calcularPrecioPlan();
+advancePlan.calcularPrecioPlan();
+proPlan.calcularPrecioPlan();
+
+// FUNCION PARA ACTUALIZAR PRECIO CUANDO CAMBIA LA CANTIDAD DE HABITACIONES
+function updatePrecio() {
+    habitaciones = document.querySelector(`#rooms`) ? Number(document.querySelector(`#rooms`).value) : 1;
+    starterPlan.calcularPrecioPlan();
+    advancePlan.calcularPrecioPlan();
+    proPlan.calcularPrecioPlan();
+    return
+}
+
+
+// EVENT LISTENER + CONDICIONAL CON ARRAY PUSH() EN LAS FUNCIONES INCLUIDAS DEL PLAN 
+let botonStarter = document.getElementById("starter.CTA");
+botonStarter.onclick = () => {
+    if (confirm("Excelente elección " + usuario +". Te gustaría agregar al plan Starter un ejecutivo de cuenta? Despues de que nos des tu respuesta te indicaremos el contenido de tu plan") == true) {
+        starterPlan.features.push("y Ejecutivo de cuenta!");
+        alert("Perfecto, las funcionalidades de tu plan "+starterPlan.nombre+" son: " + starterPlan.features +". Muchas gracias!");
+    }   else {
+        alert("Entonces tu plan "+starterPlan.nombre+" solo queda con: "+starterPlan.features + ". Gracias!");
+    }
+}
